4a8c03c0e6582ff9caefa45b7b24a967d7ca46c8
fhgggtyf <xjc18502795710@outlook.com>
2023-11-21 23:42:48 +0400

    com

--- layout/scripts.js
+++ layout/scripts.js
@@ -7,16 +7,16 @@ const main = document.querySelector('.main')
 
 
 // Function to open and close the navbar dropdown
-function toggleOpen(){
-	dropdown.classList.toggle('open')
-	main.classList.toggle('open')
-	const isOpen = dropdown.classList.contains('open')
-	if (isOpen){
-		toggleBtnIcon.classList = 'fa-solid fa-xmark'
-	}
-	else{
-		toggleBtnIcon.classList = 'fa-solid fa-bars'
-	}
+function toggleOpen() {
+  dropdown.classList.toggle('open')
+  main.classList.toggle('open')
+  const isOpen = dropdown.classList.contains('open')
+  if (isOpen) {
+    toggleBtnIcon.classList = 'fa-solid fa-xmark'
+  }
+  else {
+    toggleBtnIcon.classList = 'fa-solid fa-bars'
+  }
 }
 
 // Function to switch between tabs
@@ -33,6 +33,41 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Function to upload diary posts
+// function submitForm() {
+//   var title = document.getElementById("title").value;
+//   var mainText = document.getElementById("main-text").value;
+
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", "JournalEntry/db_write.php", true);
+//   xhr.withCredentials = true;
+//   if (xhr.readyState == 1) {
+//     alert("XHR opened successfully.");
+//   } else {
+//     console.error("Failed to open XHR.");
+//   }
+//   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//   xhr.onreadystatechange = function () {
+//     alert(xhr.status);
+//     if (xhr.readyState == 4) {
+//       if (xhr.status == 200) {
+//         console.log("Data saved successfully!");
+//         // You can perform additional actions after successful data submission
+//       } else {
+//         console.error("Error: " + xhr.responseText);
+//         // Handle errors or provide feedback to the user
+//       }
+//     }
+//   };
+
+//   // Prepare data for POST request
+//   var formData = "title=" + encodeURIComponent(title) + "&main-text=" + encodeURIComponent(mainText);
+
+//   // Send the request
+//   xhr.send(formData);
+// }
 
 // This is the same function again, to control another set of tabs on the page
 function openTab2(evt, tabName) {
